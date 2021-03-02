3d49f5930085a10f66901dd8d7694852636256f5
mcervantes71 <cervantes.martine@gmail.com>
2021-03-02 13:57:47 -0700

    Add doctors to seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,64 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# "Cardiology"
+# "Dermatology"
+# "Endocrinology"
+# "Forensic"
+# "Gastroenterology"
+# "General surgery"
+# "Geriatrics"
+# "Gynecology"
+# "Hematology"
+# "Neonatology"
+# "Neuroradiology"
+# "Obstetrics"
+# "Ophthalmology"
+# "Orthopedic"
+# "Pediatrics"
+# "Physical Medicine"
+# "Psychiatry"
+# "Radiology"
+# "Urology"
+
+d = Doctor.new
+d.first_name = "Miranda"
+d.last_name = "Bailey"
+d.medical_speciality = "General surgery"
+d.email = "mbailey@hotcakes.com"
+d.username = "mbailey"
+d.password = 123456
+d.save
+
+d = Doctor.new
+d.first_name = "Owen"
+d.last_name = "Hunt"
+d.medical_speciality = "Trauma surgery"
+d.email = "ohunt@hotcakes.com"
+d.username = "ohunt"
+d.password = 123456
+d.save
+
+d = Doctor.new
+d.first_name = "Derek"
+d.last_name = "Shepherd"
+d.medical_speciality = "Neurosurgery"
+d.email = "dshepherd@hotcakes.com"
+d.username = "dshepherd"
+d.password = 123456
+d.save
+
+d = Doctor.new
+d.first_name = "Jackson"
+d.last_name = "Avery"
+d.medical_speciality = "Plastic surgery"
+d.email = "javery@hotcakes.com"
+d.username = "javery"
+d.password = 123456
+d.save
+
+d = Doctor.new
+d.first_name = "Preston"
+d.last_name = "Burke"
+d.medical_speciality = "Cardiothoracic surgery"
+d.email = "pburke@hotcakes.com"
+d.username = "pburke"
+d.password = 123456
+d.save
