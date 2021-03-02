a4c94407e7c08244d4ab8425025dbe99c718d3ce
mcervantes71 <cervantes.martine@gmail.com>
2021-03-02 13:59:32 -0700

    Add doctors to home page

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,5 +1,39 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import doctors from '../../assets/images/doctor.png';
 
-const Home = () => (<h1 className="display-4 text-center">Med-Pro</h1>);
+const Home = () => (
+  <div>
+    <h1 className="display-4 text-center">Med-Pro</h1>
+    <div className="d-flex flex-row justify-content-around">
+      <Link
+        to="/doctors"
+        className="d-flex flex-column btn btn-light"
+        role="button"
+      >
+        <img src={doctors} alt="Doctors" title="Doctors" />
+        <span className="title">Doctors</span>
+      </Link>
+
+      <Link
+        to="/doctors"
+        className="d-flex flex-column btn btn-light"
+        role="button"
+      >
+        <img src={doctors} alt="Doctors" title="Doctors" />
+        <span className="title">Doctors</span>
+      </Link>
+
+      <Link
+        to="/doctors"
+        className="d-flex flex-column btn btn-light"
+        role="button"
+      >
+        <img src={doctors} alt="Doctors" title="Doctors" />
+        <span className="title">Doctors</span>
+      </Link>
+    </div>
+  </div>
+);
 
 export default Home;
