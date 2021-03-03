64dbed23e3d2e2dcffd9d319a87d987059d49c3b
mcervantes71 <cervantes.martine@gmail.com>
2021-03-03 15:31:43 -0700

    Add some style to doctors component

--- app/javascript/components/doctors/Doctors.jsx
+++ app/javascript/components/doctors/Doctors.jsx
@@ -24,23 +24,28 @@ class Doctors extends React.Component {
 
   render() {
     const { doctors } = this.state;
-    const allDoctors = (<table className="table">
+    const allDoctors = (<table className="table table-striped">
           <thead>
             <tr>
               <th scope="col">#</th>
-              <th scope="col">First</th>
-              <th scope="col">Last</th>
-              <th></th>
+              <th scope="col">First Name</th>
+              <th scope="col">Last Name</th>
+              <th scope="col">Email</th>
+              <th colSpan="2"></th>
             </tr>
           </thead>
         <tbody>
           {doctors.map((doctor, index) => (
-            <tr key={index}>
-              <th scope="row">{doctor.id}</th>
-              <td>{doctor.first_name}</td>
-              <td>{doctor.last_name}</td>
-              <td></td>
-            </tr>))}
+              <tr key={index}>
+                <th scope="row">{doctor.id}</th>
+                <td>{doctor.first_name}</td>
+                <td>{doctor.last_name}</td>
+                <td>{doctor.email}</td>
+                <td><Link type="button" className="btn btn-outline-primary" to={`/doctor/${doctor.id}`}>Show <i class="fas fa-user"></i></Link></td>
+                <td><Link type="button" className="btn btn-outline-secondary" to="/">Edit <i className="fas fa-edit"></i></Link></td>
+                <td><Link type="button" className="btn btn-outline-danger" to="/">Delete <i className="fas fa-trash-alt"></i></Link></td>
+              </tr>
+          ))}
         </tbody>
       </table>
     );
@@ -55,11 +60,11 @@ class Doctors extends React.Component {
 
     return (
       <>
-        <section className="jumbotron jumbotron-fluid text-center">
+        <div className="jumbotron jumbotron-fluid text-center">
           <div className="container py-5">
           <h1 className="display-4">Doctors</h1>
           </div>
-        </section>
+        </div>
         <div className="py-5">
           <main className="container">
             <div className="text-right mb-3">
