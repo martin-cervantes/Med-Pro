5e1626e3094afd07981b3ff19e764c37e8878dc0
mcervantes71 <cervantes.martine@gmail.com>
2021-03-10 12:07:06 -0700

    Add bootstrap configuration

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,8 @@ import * as ActiveStorage from '@rails/activestorage';
 import 'channels';
 import "@fortawesome/fontawesome-free/css/all"
 
+import "bootstrap"
+
 Rails.start();
 Turbolinks.start();
 ActiveStorage.start();
